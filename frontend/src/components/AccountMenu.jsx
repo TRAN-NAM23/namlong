0d271b7dac144e50f9bd7a53ca5a3e4b889f0029
Trần Phương Nam <your-email@example.com>
2026-03-13 14:03:46 +0700

    Change axios import to custom instance

--- frontend/src/components/AccountMenu.jsx
+++ frontend/src/components/AccountMenu.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import axios from "../config/api";
 import "./AccountMenu.css";
 
 const AccountMenu = ({ user, onLogout, onOpenPopUp }) => {
