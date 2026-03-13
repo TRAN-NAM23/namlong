41425e951cd793ed4101b764a86bbbf14f4f800c
Trần Phương Nam <your-email@example.com>
2026-03-13 14:10:29 +0700

    Change axios import to custom instance

--- frontend/src/config /api.js
+++ frontend/src/config /api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://namlong-hdi8.onrender.com', // Thay bằng link Render này
+  baseURL: 'https://namlong-hdi8.onrender.com', // Link backend của bạn
+  headers: {
+    'Content-Type': 'application/json'
+  }
 });
 
 export default instance;
