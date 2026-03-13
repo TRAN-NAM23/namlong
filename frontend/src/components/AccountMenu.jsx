90e6e50ae5052e6ca9f36fcddb3f64a2bcbc8953
Trần Phương Nam <your-email@example.com>
2026-03-13 14:47:48 +0700

    Fix: force update file case sensitivity for Netlify

--- frontend/src/components/AccountMenu.jsx
+++ frontend/src/components/AccountMenu.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
-import api from "../config/api.js";
+import api from "../config/api";
 import "./AccountMenu.css";
 
 const AccountMenu = ({ user, onLogout, onOpenPopUp }) => {
